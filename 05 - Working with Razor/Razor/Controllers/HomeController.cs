11e6c85c67210f0ad51767fe7f1a83ee6727bd12
Karina Belikova <kiit15.belikova@gmail.com>
2019-06-17 19:18:35 +0300

    chapter 5, 6

---
+++ 05 - Working with Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region ViewResult
+        //public ViewResult Index()
+        //{
+        //    Product myProduct = new Product
+        //    {
+        //        ProductID = 1,
+        //        Name = "Kayak",
+        //        Description = "A boat for one person",
+        //        Category = "Watersports",
+        //        Price = 275M
+        //    };
+
+        //    return View(myProduct);
+        //}
+        #endregion
+
+        #region IActionResult
+        public IActionResult Index()
+        {
+            Product[] array =
+            {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer ball", Price = 19.50M },
+                new Product { Name = "Corner flag", Price = 34.95M }
+            };
+
+            return View(array);
+        }
+        #endregion
+    }
+}
---
+++ 06 - Working with VS/WorkingWith/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkingWith.Models;
+using System.Linq;
+
+namespace WorkingWith.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+            => View(SimpleRepository.SharedRepository.Products.Where(p => p.Price < 50));
+    }
+}
